3efecbcf34868658b7c5ed1f1b905085219327b0
unknown <mvishnu911@gmail.com>
2022-02-23 23:32:32 +0530

    added pwa functionality

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
+);
+
+workbox.routing.registerRoute(
+  new RegExp(".png$"),
+  workbox.strategies.cacheFirst({
+    cacheName: "images-cache",
+  })
+);
